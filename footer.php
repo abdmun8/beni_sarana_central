1b6935d22a8254719011c96a91b8c397f19ce1b4
abdmun8 <abdmun8@gmail.com>
2017-05-17 21:22:02 +0700

    css, js, header, index, footer

---
+++ footer.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <style type="text/css">
+  	ul li a{
+  		text-decoration: none;
+  		color: white;
+  		a:hover: white;
+  	}
+  	ul li a:hover{
+  		color: white;
+  	}
+  	ul li{
+  		list-style:  none;
+  	}
+  </style>
+  <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+  <script src="asset/js/jquery.min.js"></script>
+  <script src="asset/js/bootstrap.min.js" ></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-bottom">
+  <div class="container" style="padding-top: 20px;">
+    <div class="col-md-3">
+		<ul>
+			<li><a href="#">About us</a></li>
+			<li><a href="#">Contact & support</a></li>
+			<li><a href="#">Enterprise</a></li>
+			</ul>
+	</div>
+
+	<div class="col-md-12"><p style="color: #ffffff; text-align: center; padding-top: 10px;">&copy PT Sarana Sentral bajatama Tbk. <?php echo date('Y');?></p></div>
+  </div>
+</nav>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'header.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PT Sarana Sentral Bajatama, Tbk.</title>
+  <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+  <script src="asset/js/jquery.min.js"></script>
+  <script src="asset/js/bootstrap.min.js" ></script>
+</head>
+<body>
+<div class="container">
+	<div style="padding-top: 30px;">
+	<h1 style="text-align: center;">Selamat Datang</h1>
+	<p align="center">Disini anda dapat melihat jadwal, laporan dan stock material</p>
+	<div class="col-md-4">
+		jadwal
+	</div>
+	<div class="col-md-4">
+		Laporan
+	</div>
+	<div class="col-md-4">
+		Stock
+	</div>
+</div>
+</div>
+</body>
+</html>
+<?php 
+require_once 'footer.php';
+ ?>
